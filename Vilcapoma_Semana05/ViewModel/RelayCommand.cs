618d10ad13b05e2812b2d00432ce8cc6f89bdb51
MarvinVilcapoma <friman.vilcapoma@tecsup.edu.pe>
2020-11-04 23:46:51 -0500

    Commit Update

---
+++ Vilcapoma_Semana05/ViewModel/RelayCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Vilcapoma_Semana05.ViewModel
+{
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+        #endregion
+        #region Constructors
+
+        ///<summary>
+        /// Initializes a new instance of <see cref="DelegateCommand{T}"/>.
+        /// </summary>
+        /// <param name="execute">Delegate to execute when Execute is called on the command</param>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        #endregion
+    }
+}
